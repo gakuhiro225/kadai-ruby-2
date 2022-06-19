b66af5235ba0d2b1a645ea42ece522167430d3ba
gaku.hiro <gakuhiro225@gmail.com>
2022-06-19 06:33:39 +0000

    kadai-ruby-2

---
+++ kadai-fizzbuzz.rb
@@ -0,0 +1,21 @@
+num_max = 100
+
+(1..num_max).each do |num|
+
+def fizzbuzz(num)
+
+ if (num%3==0 && num%5==0)
+  return "FizzBuzz"
+ elsif num%3 == 0
+  return "Fizz"
+ elsif num%5==0
+  return "Buzz" 
+ else
+  return num
+ end
+end
+
+# 以下は動作確認用の記述です
+puts fizzbuzz(num)
+
+end
